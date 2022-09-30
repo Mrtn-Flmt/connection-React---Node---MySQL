5999c7f2f83a7b2a42c4389ffbc7d32db2e3d20c
Mrtn-Flmt <mrtn.flmt@gmail.com>
2022-09-30 12:06:35 +0200

    [+] rework margin & padding

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
 
-const Navigation = () => {
+export default Navigation = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-light">
-            <div className="container-fluid">
+            <div className="container d-flex">
                 <a className="navbar-brand">employ me 😎</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
                     <ul className="navbar-nav">
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page"><NavLink to="/">Home</NavLink></a>
@@ -20,11 +20,6 @@ const Navigation = () => {
                     </ul>
                 </div>
             </div>
-            <br/>
-            <br/>
-            <br/>
         </nav>
     );
 };
-
-export default Navigation;
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -3,17 +3,20 @@ import React from 'react';
 const Search = () => {
     return (
         <nav className="navbar bg-primary">
+            <div class="container">
+                
             <form className="row col-12 col-sm-12 col-md-12 justify-content-center">
-                <div className="row col-xs-12 col-sm-12 col-md-5">
+                <div className="row col-xs-12 col-sm-12 col-md-5 p-3">
                     <input className="form-control" type="search" placeholder="Quoi ?" aria-label="Search" />
                 </div>
-                <div className="row col-xs-12 col-sm-12 col-md-5">
+                <div className="row col-xs-12 col-sm-12 col-md-5 p-3">
                     <input className="form-control" type="search" placeholder="Où ?" aria-label="Search" />
                 </div>
-                <div className="row col-3 col-sm-3 col-md-2">
-                    <button className="btn btn-secondary" type="submit">Search</button>
+                <div className="d-flex justify-content-center">
+                    <button className="btn btn-dark" type="submit">Search</button>
                 </div>
             </form>
+            </div>
         </nav>
     );
 };
